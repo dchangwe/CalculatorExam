f6568e0e4f53c439e0da30f6e7500ac20940fb04
dana <dana.changwe@szi.gov.zm>
2020-01-15 09:43:13 +0200

    Feature : edited the Variance

--- StatsVariance/IFindVariance.cs
+++ StatsVariance/IFindVariance.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace StatisticsVariance
+namespace StatsVariance
 {
-    public interface  IFindVariance
+    public interface IFindVariance
     {
         public double Variance(dynamic arrayA);
 
