8be7f9a35ebd7cb9a3fb24d066e9c9fa826eb3d5
MwangoS <natchaziya@gmail.com>
2020-01-15 11:00:05 +0200

    Task: Created Division operation

---
+++ Division/Division.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Division
+{
+    public class Division
+    { 
+     public static int Quotient(int a, int b)
+     {
+        return a / b;
+     }
+
+     public static double Quotient(double a, double b)
+     {
+        return a / b;
+     }
+
+     public static double Quotient(double[] a)
+     {
+        double c = a[0];
+        for (int i = 1; i < a.Length; i++)
+        {
+            c = Quotient(c, a[i]);
+        }
+        return c;
+     }
+
+     public static double Quotient(int[] a)
+     {
+        double c = a[0];
+        for (int i = 1; i < a.Length; i++)
+        {
+            c = Quotient(c, a[i]);
+        }
+        return c;
+     }
+    }
+}
